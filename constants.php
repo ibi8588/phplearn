7c058b7e1d6d9305651b107590783b50a59c9ca7
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2017-11-14 22:04:23 -0800

    created constants page

---
+++ constants.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My Third PHP page</h1>
+
+<?php
+define("GREETING", "Hello :)");
+define("person", "Ibrahim");
+echo GREETING;
+echo "<br>";
+echo person;
+?>
+
+</body>
+</html>
