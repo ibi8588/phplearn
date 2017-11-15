928b528b274d8ce67552af94a585371cad41b651
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2017-11-14 21:34:38 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$cars = array("Volvo","BMW","Toyota");
+print var_dump($cars);
+?>
+
+</body>
+</html>
---
+++ object.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+class Car {
+    function Car() {
+        $this->model = "Camery";
+        $this->make = "Prius";
+    }
+}
+
+$cam = new Car();
+
+echo $cam->model;
+echo "<br>";
+echo $cam->make;
+?>
+
+</body>
+</html>
