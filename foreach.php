3bed769545b6ff9aa5a5a4f93a7b7d2add38e99e
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2017-11-14 23:24:22 -0800

    added two more files

---
+++ foreach.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My Third PHP page</h1>
+
+<?php
+ $cars = array("Volvo", "BMW", "Toyota");
+foreach ($cars as$value ){
+  print "$value <br>";
+}
+?>
+
+</body>
+</html>
---
+++ forloop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My Third PHP page</h1>
+
+<?php
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+}
+?>
+
+</body>
+</html>
